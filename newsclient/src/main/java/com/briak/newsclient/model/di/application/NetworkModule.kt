5b236d8d80d217bad985d668be6342097c3f12b8
Briak <efros.christina@gmail.com>
2018-09-21 11:42:02 +0300

    Fix handshake error

--- newsclient/src/main/java/com/briak/newsclient/model/di/application/NetworkModule.kt
+++ newsclient/src/main/java/com/briak/newsclient/model/di/application/NetworkModule.kt
@@ -18,24 +18,60 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.*
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
+import com.briak.newsclient.R
+import java.security.KeyStore
+import java.security.cert.CertificateFactory
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.TrustManagerFactory
+import javax.net.ssl.X509TrustManager
+
 
 @Module
 class NetworkModule {
     @Provides
     @Singleton
-    fun provideOkHttpClient(context: Context): OkHttpClient =
-            with(OkHttpClient.Builder()) {
-                cache(Cache(context.cacheDir, 20 * 1024))
-                connectTimeout(30, TimeUnit.SECONDS)
-                readTimeout(30, TimeUnit.SECONDS)
+    fun provideOkHttpClient(context: Context): OkHttpClient {
+        val certificateFactory = CertificateFactory.getInstance("X.509")
 
-                if (BuildConfig.DEBUG) {
-                    addNetworkInterceptor(
-                            HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
-                    )
-                }
-                build()
+        val inputStream = context.resources.openRawResource(R.raw.ssl_certificate) //(.crt)
+        val certificate = certificateFactory.generateCertificate(inputStream)
+        inputStream.close()
+
+        // Create a KeyStore containing our trusted CAs
+        val keyStoreType = KeyStore.getDefaultType()
+        val keyStore = KeyStore.getInstance(keyStoreType)
+        keyStore.load(null, null)
+        keyStore.setCertificateEntry("ca", certificate)
+
+        // Create a TrustManager that trusts the CAs in our KeyStore.
+        val tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm()
+        val trustManagerFactory = TrustManagerFactory.getInstance(tmfAlgorithm)
+        trustManagerFactory.init(keyStore)
+
+        val trustManagers = trustManagerFactory.trustManagers
+        val x509TrustManager = trustManagers[0] as X509TrustManager
+
+
+        // Create an SSLSocketFactory that uses our TrustManager
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, arrayOf<TrustManager>(x509TrustManager), null)
+        val sslSocketFactory = sslContext.socketFactory
+
+        return with(OkHttpClient.Builder()) {
+            cache(Cache(context.cacheDir, 20 * 1024))
+            connectTimeout(30, TimeUnit.SECONDS)
+            readTimeout(30, TimeUnit.SECONDS)
+            sslSocketFactory(sslSocketFactory, x509TrustManager)
+
+            if (BuildConfig.DEBUG) {
+                addNetworkInterceptor(
+                        HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+                )
             }
+            build()
+        }
+    }
 
     @Provides
     @Singleton
